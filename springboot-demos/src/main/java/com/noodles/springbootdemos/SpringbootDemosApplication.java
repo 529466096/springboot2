7d631a62f2ce13fb62fa3985de373ac35dd621e4
Lid23 <645465550@qq.com>
2022-01-21 18:13:29 +0800

    Springboot实例

--- springboot-demos/src/main/java/com/noodles/springbootdemos/SpringbootDemosApplication.java
+++ springboot-demos/src/main/java/com/noodles/springbootdemos/SpringbootDemosApplication.java
@@ -1,8 +1,14 @@
 package com.noodles.springbootdemos;
 
+import java.sql.Connection;
+
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -15,6 +21,24 @@ public class SpringbootDemosApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         System.out.println("服务已启动");
+    }
+
+    /**
+     * 配置同时支持http和https
+     * @author 巫威
+     * @date 2022/1/21 16:49
+     */
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector  connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
     }
 
 }
